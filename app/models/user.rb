2d6b8136e672bf89eaa4f06472520d9b7b4957db
Ali Alrawi <ali.developer90@gmail.com>
2017-01-16 23:47:20 +0300

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions ,through: :rides
+  #validates_presence_of :name, :happiness, :nausea, :height, :tickets
+
+  def mood
+  	self.happiness > self.nausea ? "happy" : "sad" if self.happiness && self.nausea
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :attractions ,only: [:index,:show,:new,:create,:update,:edit]
 
+  root 'application#home'
+  resources :users
+  get "/signin" ,to: 'sessions#new'
+  get "/logout" ,to: 'sessions#destroy'
+  post "/rides" ,to: 'rides#create'
+  resources :sessions, only: [:create]
 end
